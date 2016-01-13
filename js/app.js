a73c3a13a7dea53e3090dec312801a65c6a7bc51
yotter <hollyyotter11@gmail.com>
2016-01-13 01:54:15 -0500

    almost final

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function () {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function () {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function () {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+			);
+	})
+	.mouseup(function () {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+	$(document).keydown(function (event) {
+		if (event.which == 88) {
+			$('ryu.still').hide();
+			$('ryu.ready').hide();
+			$('ryu.cool').show();
+		}
+	});
+	$(document).keyup(function (event) {
+		if (event.which == 88) {
+			$('ryu.cool').hide();
+			$('ryu.still').show();
+		}
+	});
+});
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
